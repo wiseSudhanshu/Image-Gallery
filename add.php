bbc3fd9bf9f51932cc6d27a6631cd862d4c3b06d
Sudhanshu Rai <sudhanshu.rai@wisdmlabs.com>
2023-04-24 09:20:14 +0530

    added an add.php file with a form to take user input

---
+++ add.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Sriracha&display=swap" rel="stylesheet">
+      <title>Add Image</title>
+  </head>
+  <body>
+    <!-- <h1>Add Image</h1> -->
+
+    <form action="postAction.php" method="post" enctype="multipart/form-data">
+      <div class="form-group">
+        <label for="title">Title:</label>
+        <input type="text" id="title" required name="title" class="form-control">
+      </div>
+      
+      <div class="form-group">
+        <label for="filename">Image:</label>
+        <input type="file" id="filename" required name="filename" class="form-control-file">
+      </div>
+
+      <input type="submit" value="Upload" name="submit" class="btn btn-primary">
+    </form>
+  </body>
+</html>
